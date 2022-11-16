ecaf7815da704523a14c034a9ff08768e8ea5c43
DESKTOP-QIUUF47\Admin <jihoon708@naver.com>
2022-11-16 22:17:44 +0900

    Edit Api Interface

--- app/src/main/java/com/odom/briefweatherinfo/util/CurrentWeatherApi.kt
+++ app/src/main/java/com/odom/briefweatherinfo/util/CurrentWeatherApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 
 interface CurrentWeatherApi {
 
-    @GET("https://api.openweathermap.org/data/2.5/weather")
+    @GET("/data/2.5/weather")
     fun getCurrentWeather(
         @Query("lat") lat: Double?,
         @Query("lon") lon: Double?,
