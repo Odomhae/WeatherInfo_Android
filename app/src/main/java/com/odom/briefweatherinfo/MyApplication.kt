a2ebbb9c0b3887b741232c98657de18288ca0c16
이지훈 <jhlee@nicevan.co.kr>
2022-11-18 17:40:14 +0900

    Get LatestVersion from Remote Config

--- app/src/main/java/com/odom/briefweatherinfo/MyApplication.kt
+++ app/src/main/java/com/odom/briefweatherinfo/MyApplication.kt
@@ -3,6 +3,12 @@ package com.odom.briefweatherinfo
 import io.realm.RealmConfiguration
 
 import android.app.Application
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import io.realm.Realm
 
 
@@ -17,5 +23,32 @@ class MyApplication : Application() {
                                             // 그럴 필요 없이 자동으로 기존의 realm 데이터를 삭제하여 앱이 동작하게 해줌
             .build()
         Realm.setDefaultConfiguration(config)
+
+        initRemoteConfig()
+    }
+
+    private fun initRemoteConfig() {
+
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+
+        //todo
+        val version = remoteConfig.getString("latest_version")
+        //Log.d("===ss", version)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                   // Log.d("===ss", "Fetch succeeded: $updated")
+                } else {
+                    //Log.d("===ss", "Fetch failed")
+                }
+            }
+
     }
 }
